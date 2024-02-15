62582adc25324d6f9dbcf4b1fd65c8ce96ba5cc8
puranik28 <135043884+puranik28@users.noreply.github.com>
2024-02-14 21:37:58 -0700

    Update counter.model.js

--- models/counter.model.js
+++ models/counter.model.js
@@ -9,23 +9,23 @@ const CounterSchema = new mongoose.Schema({
 const Counter = mongoose.model("Counter", CounterSchema);
 
 export async function getNextIncrementalId() {
-    try {
-      const lastMonster = await Monster.findOne().sort({ id: -1 });
-      let nextId;
-      if (lastMonster) {
-        nextId = lastMonster.id + 1;
-      } else {
-        nextId = 1;
-      }
-      await Counter.findOneAndUpdate(
-        { _id: 'monsterId' }, 
-        { $set: { sequence_value: nextId } },
-        { upsert: true }
-      );
-      return nextId;
-    } catch (e) {
-      throw new Error(`Failed to generate incremental ID: ${e.message}`);
+  try {
+    const lastMonster = await Monster.findOne().sort({ id: -1 });
+    let nextId;
+    if (lastMonster) {
+      nextId = lastMonster.id + 1;
+    } else {
+      nextId = 1;
     }
+    await Counter.findOneAndUpdate(
+      { _id: 'monsterId' }, 
+      { $set: { sequence_value: nextId } },
+      { upsert: true }
+    );
+    return nextId;
+  } catch (e) {
+    throw new Error(`Failed to generate incremental ID: ${e.message}`);
+  }
 }
 
 export default Counter;
